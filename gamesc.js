eac30a7164700808aa322780ca67676db13d4c98
123Lniner <164357272+123Lniner@users.noreply.github.com>
2024-04-24 07:38:39 -0400

    Add files via upload

---
+++ gamesc.js
@@ -0,0 +1,8 @@
+// two slashes without any text after it is just a placeholder
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+//
+canvas.width = 1024;
+canvas.height = 576;
+//
+c.fillRect(0, 0, canvas.width, canvas.height);
